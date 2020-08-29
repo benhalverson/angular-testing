bc6ac0f5e6f9d60e7892471595b690db49674ac0
Ben Halverson <benhalverson33@gmail.com>
2020-08-28 23:25:39 -0700

    basic tests for header

--- src/app/src/components/header/header.component.spec.ts
+++ src/app/src/components/header/header.component.spec.ts
@@ -22,4 +22,25 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show logo', () => {
+      expect(fixture.nativeElement.querySelector('[data-test="logo"]')).toBeTruthy();
+  });
+
+  it('should show logo', () => {
+      expect(fixture.nativeElement.querySelector('[data-test="search"]')).toBeTruthy();
+  });
+
+  it('should search bar', () => {
+      expect(fixture.nativeElement.querySelector('[data-test="menu"]')).toBeTruthy();
+  });
+
+  it('should show filters', () => {
+      expect(fixture.nativeElement.querySelector('[data-test="home-type"]')).toBeTruthy();
+      expect(fixture.nativeElement.querySelector('[data-test="dates"]')).toBeTruthy();
+      expect(fixture.nativeElement.querySelector('[data-test="guests"]')).toBeTruthy();
+      expect(fixture.nativeElement.querySelector('[data-test="price"]')).toBeTruthy();
+      expect(fixture.nativeElement.querySelector('[data-test="rooms"]')).toBeTruthy();
+      expect(fixture.nativeElement.querySelector('[data-test="amenities"]')).toBeTruthy();
+  });
 });
