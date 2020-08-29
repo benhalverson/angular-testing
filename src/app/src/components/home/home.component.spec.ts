0704e3b61337225097fd84c6e14b70d601e9f3c1
Ben Halverson <benhalverson33@gmail.com>
2020-08-29 00:35:20 -0700

    adding data service

--- src/app/src/components/home/home.component.spec.ts
+++ src/app/src/components/home/home.component.spec.ts
@@ -19,7 +19,22 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show homes', () => {
+    expect(fixture.nativeElement.querySelectorAll('[data-test="home"]').length).toBe(3);
+    expect(fixture.nativeElement.querySelectorAll('[data-test="home"]').length).toBe(3);
   });
+
+  it('should show homes info', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="title"]').innerText).toEqual('Home 1');
+    expect(fixture.nativeElement.querySelector('[data-test="location"]').innerText).toEqual('new york');
+    expect(fixture.nativeElement.querySelector('[data-test="image"]')).toBeTruthy();
+  });
+
+  it('should show homes info', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="title"]').innerText).toEqual('Home 1');
+    expect(fixture.nativeElement.querySelector('[data-test="location"]').innerText).toEqual('new york');
+    expect(fixture.nativeElement.querySelector('[data-test="image"]')).toBeTruthy();
+  });
+
+
 });
--- src/app/src/components/home/home.component.ts
+++ src/app/src/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import {DataService} from '../../services/data.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  homes$;
+  constructor(private readonly dataService: DataService,) { }
 
   ngOnInit(): void {
-  }
+      this.homes$ = this.dataService.getHomes$();
+   /*this.homes$ = of([
+    {
+        title: 'Home 1',
+        image: 'assest/listing.jpg',
+        location: 'new york'
+    },
+    {
+        title: 'Home 2',
+        image: 'assest/listing.jpg',
+        location: 'san jose'
+    },
+    {
+        title: 'Home 3',
+        image: 'assest/listing.jpg',
+        location: 'san francisco'
+    }
+   ])*/
+    }
 
 }
